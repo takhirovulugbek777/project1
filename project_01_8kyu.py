0fb0050c99d5b6eafb08f3251e918c6ed9dbfe9f
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-04-10 20:50:55 +0500

    project1

---
+++ project_01_8kyu.py
@@ -0,0 +1,12 @@
+"""
+    Create a function that takes an integer as an argument and returns
+    "Even" for even numbers or "Odd" for odd numbers.
+"""
+
+num = int(input('enter a number: '))
+
+if (num % 2) == 0:
+    print('number is even')
+else:
+    print('numer is odd')
+
